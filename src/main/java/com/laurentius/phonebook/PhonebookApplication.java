3890d8e1275b3d8f3064807cc478ef7974a9eb7b
Laurentius Andre <laurentius.andre@anabatic.com>
2024-02-04 14:04:57 +0700

    Cleanup main class

--- src/main/java/com/laurentius/phonebook/PhonebookApplication.java
+++ src/main/java/com/laurentius/phonebook/PhonebookApplication.java
@@ -1,16 +1,7 @@
 package com.laurentius.phonebook;
 
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
-import org.springframework.core.type.classreading.MetadataReader;
-import org.springframework.core.type.classreading.MetadataReaderFactory;
-import org.springframework.core.type.filter.TypeFilter;
 
 @SpringBootApplication
 public class PhonebookApplication {
@@ -18,23 +9,4 @@ public class PhonebookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PhonebookApplication.class);
 	}
-
-//	@Bean
-//	public BeanFactoryPostProcessor beanFactoryPostProcessor(ApplicationContext beanRegistry) {
-//		return beanFactory -> genericApplicationContext(
-//				(BeanDefinitionRegistry) ((AnnotationConfigServletWebServerApplicationContext) beanRegistry)
-//						.getBeanFactory());
-//	}
-//
-//	private void genericApplicationContext(BeanDefinitionRegistry beanRegistry) {
-//		ClassPathBeanDefinitionScanner beanDefinitionScanner = new ClassPathBeanDefinitionScanner(beanRegistry);
-//		beanDefinitionScanner.addIncludeFilter(removeModelAndEntitiesFilter());
-//		beanDefinitionScanner.scan("com.laurentius.phonebook");
-//	}
-//
-//	public static TypeFilter removeModelAndEntitiesFilter() {
-//		return (MetadataReader mr, MetadataReaderFactory mrf) -> !mr.getClassMetadata()
-//				.getClassName()
-//				.endsWith("Model");
-//	}
 }
